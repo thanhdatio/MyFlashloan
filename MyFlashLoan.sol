f07e19db7c7120d9a458137916b0579c3adc44d9
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2022-03-11 13:00:28 +0700

    Update MyFlashLoan.sol

--- MyFlashLoan.sol
+++ MyFlashLoan.sol
@@ -46,4 +46,35 @@ contract MyV2FlashLoan is FlashLoanReceiverBase {
         
         return true;
     }
+    
+    function myFlashLoanCall() public {
+        address receiverAddress = address(this);
+
+        address[] memory assets = new address[](2);
+        assets[0] = address(INSERT_ASSET_ONE_ADDRESS);
+        assets[1] = address(INSERT_ASSET_TWO_ADDRESS);
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = INSERT_ASSET_ONE_AMOUNT;
+        amounts[1] = INSERT_ASSET_TWO_AMOUNT;
+
+        // 0 = no debt, 1 = stable, 2 = variable
+        uint256[] memory modes = new uint256[](2);
+        modes[0] = INSERT_ASSET_ONE_MODE;
+        modes[1] = INSERT_ASSET_TWO_MODE;
+
+        address onBehalfOf = address(this);
+        bytes memory params = "";
+        uint16 referralCode = 0;
+
+        LENDING_POOL.flashLoan(
+            receiverAddress,
+            assets,
+            amounts,
+            modes,
+            onBehalfOf,
+            params,
+            referralCode
+        );
+    }
 }
